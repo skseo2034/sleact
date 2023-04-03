61d799f38c9918358187c12bf990f501e1491c0d
서수교 <sk.seo@navercorp.com>
2023-04-03 15:16:04 +0900

    2023-04-03 : 학습

--- alecture/components/ChatList/ChatList.tsx
+++ alecture/components/ChatList/ChatList.tsx
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { ChatZone, Section } from '@components/ChatList/chatListStyles';
+import Scrollbars from 'react-custom-scrollbars-2';
+import Chat from '@components/Chat/Chat';
+import dayjs from 'dayjs';
 
 const ChatList = () => {
+	const scrollbarRef = useRef(null);
+	const onscroll = () => {
+		//.....
+	};
 	return (
 		<ChatZone>
-			<Section>section</Section>
+			<Scrollbars autoHide ref={scrollbarRef} onScrollFrame={onscroll}>
+				<Section>
+					section&nbsp;&nbsp;
+					<span>{dayjs('20230131235959').format('h:mm A')}</span>
+				</Section>
+				<Section>
+					section&nbsp;&nbsp;
+					<span>{dayjs('20230131235959').format('YYYY-MM-DDTHH:mm:ssZ')}</span>
+				</Section>
+				<Section>section</Section>
+				<Section>section</Section>
+				<Section>section</Section>
+				<Section>section</Section>
+				<Section>section</Section>
+				<Section>section</Section>
+				<Section>section</Section>
+				<Section>section</Section>
+				<Section>section</Section>
+				<Section>section</Section>
+				<Section>section</Section>
+				<Section>section</Section>
+				<Section>section</Section>
+				<Section>section</Section>
+				<Section>section</Section>
+				<Section>section</Section>
+				<Section>section</Section>
+				<Section>section</Section>
+				<Section>section</Section>
+				<Section>section</Section>
+				<Section>section</Section>
+				<Section>section</Section>
+				<Section>section</Section>
+				<Section>section</Section>
+				<Section>section</Section>
+				<Section>section</Section>
+				<Section>section</Section>
+				<Section>section</Section>
+				<Section>section</Section>
+				<Section>section</Section>
+				<Section>section</Section>
+				<Section>section</Section>
+			</Scrollbars>
 		</ChatZone>
 	);
 };
